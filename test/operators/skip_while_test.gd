f556cabcf1ccc7af1239c4d17979f44e3e1d767c
Minami Tomonobu <minami@edanoue.com>
2025-01-28 11:03:10 +0900

    Refactor skip_while operator to use a dedicated observer class for improved readability and maintainability; add tests for multiple subscribers.

--- test/operators/skip_while_test.gd
+++ test/operators/skip_while_test.gd
@@ -14,3 +14,27 @@ func test_standard() -> void:
 	subject.on_next(1)
 	subject.on_next(2)
 	assert_array(result, true).is_equal([3, 1, 2])
+
+func test_two_subscribers() -> void:
+	var result1 := []
+	var result2 := []
+	var subject := Subject.new()
+	var skip_while := subject.skip_while(func(x): return x <= 2)
+
+	skip_while.subscribe(func(x): result1.push_back(x))
+
+	subject.on_next(1)
+	subject.on_next(3)
+	subject.on_next(1)
+
+	assert_array(result1, true).is_equal([3, 1])
+	assert_array(result2, true).is_equal([])
+
+	skip_while.subscribe(func(x): result2.push_back(x))
+
+	subject.on_next(1)
+	subject.on_next(3)
+	subject.on_next(1)
+
+	assert_array(result1, true).is_equal([3, 1, 1, 3, 1])
+	assert_array(result2, true).is_equal([3, 1])
